325592f5db10616f912c9dbeaef27b7ed4568a74
Yamini Devi P <yaminiprakashkumar@gmail.com>
2026-07-07 23:07:55 +0530

    Delete Back_end/Hands_on_1 directory

--- Back_end/Hands_on_1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path  # type: ignore[import]
-
-from .views import hello_view
-
-urlpatterns = [
-    path('hello/', hello_view, name='hello'),
-]
--- Back_end/Hands_on_1/views.py
+++
@@ -1,7 +0,0 @@
-import importlib
-
-django_http = importlib.import_module("django.http")
-HttpResponse = django_http.HttpResponse
-
-def hello_view(request):
-    return HttpResponse("Course Management API is running")
